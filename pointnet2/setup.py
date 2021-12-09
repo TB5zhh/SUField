efda467d50d9454209ebe8ea4e39f076e30fffe1
tb5zhh <tianbeiwen88@gmail.com>
2021-12-09 16:54:22 +0800

    finally solve the setup of pointnet2

---
+++ pointnet2/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import glob
+
+_ext_src_root = "_ext_src"
+_ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
+    "{}/src/*.cu".format(_ext_src_root)
+)
+
+setup(
+    name='pointnet2',
+    ext_modules=[
+        CUDAExtension(
+            name='pointnet2._ext',
+            sources=_ext_sources,
+        )
+    ],
+    include_dirs=[f"{_ext_src_root}/include"],
+    cmdclass={
+        'build_ext': BuildExtension
+    },
+)
